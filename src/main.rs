1bdb80d0001ab966fea2339772370e7cd2db6e70
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2024-10-31 15:44:57 +0100

    Deck of cards, impl, structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+        Deck { cards }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let deck = Deck::new();
+    println!("Heres is your deck {:#?}", deck);
 }
