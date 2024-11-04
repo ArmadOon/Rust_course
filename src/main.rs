662bb34e8440683178f77a5561fc00d9b7cbad2e
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2024-11-04 18:57:54 +0100

    string slices book

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 fn main() {
-    fn first_word(s: &String) -> usize {
-        let bytes = s.as_bytes();
-        for (i, &item) in bytes.iter().enumerate() {
-            if (item == b' ') {
-                return i;
-            }
-        }
-        s.len()
-    }
-    let mut s = String::from("He llo world!");
-    let word = first_word(&s);
-    s.clear();
-    println!("{:#?}", word);
+    let s = String::from("Hello World");
+    let hello = &s[0..5];
+    let world = &s[6..];
+
+    println!("{:#?}", hello);
+    println!("{:#?}", world);
 }
