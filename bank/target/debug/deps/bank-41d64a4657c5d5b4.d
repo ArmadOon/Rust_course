699db775156f8796f387e009d2350b136ff8841e
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2024-11-05 13:19:38 +0100

    structs for bank

---
+++ bank/target/debug/deps/bank-41d64a4657c5d5b4.d
@@ -0,0 +1,5 @@
+/Users/pluhy/Documents/Rust/udemy/Learning_rust/grinders_course/course/bank/target/debug/deps/libbank-41d64a4657c5d5b4.rmeta: src/main.rs
+
+/Users/pluhy/Documents/Rust/udemy/Learning_rust/grinders_course/course/bank/target/debug/deps/bank-41d64a4657c5d5b4.d: src/main.rs
+
+src/main.rs:
---
+++ bank/target/debug/deps/bank-6a29537b36a418a1.d
@@ -0,0 +1,5 @@
+/Users/pluhy/Documents/Rust/udemy/Learning_rust/grinders_course/course/bank/target/debug/deps/libbank-6a29537b36a418a1.rmeta: src/main.rs
+
+/Users/pluhy/Documents/Rust/udemy/Learning_rust/grinders_course/course/bank/target/debug/deps/bank-6a29537b36a418a1.d: src/main.rs
+
+src/main.rs:
